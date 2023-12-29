e4896f34c5e10906d86f5f191337dda3838b7933
Chung Hyunwoo <woo980711@naver.com>
2023-12-29 17:26:10 +0900

    📦 Rename: Moving Font Files
    Go inside the Font file assets folder
    Update font file path

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import "./Fonts/Fonts.css";
+import "./assets/Font/Font.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
