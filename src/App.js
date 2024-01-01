381703e96970c4c0d41c7cb79c3fd1349dd206e7
odukong <ohbin1017@gmail.com>
2024-01-01 14:10:28 +0900

    :sparkles: Dev: "로그인 전 홈페이지 구현"
    프로젝트 리스트 및 상세 페이지 구현
    
    Resolves: #5

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import { Route, Routes, BrowserRouter, Link } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Register from "./pages/Register";
 import styled from "styled-components";
 import Home from "./pages/Home";
+import ProjectDetail from "./components/ProjectDetail";
+import Navbar from "./common/Navbar/Navbar";
 
 function App() {
   const Headers = styled.div`
     display: flex;
+    padding:3.9rem 0;
   `;
 
   return (
     <BrowserRouter>
       <Headers>
-        <Link to="/">Home</Link>
-      </Headers>
+          <Navbar/>
+        </Headers>
       <Routes>
         <Route path="/*" element={<Home />} />
+        <Route path="/Home/project/*" element={<ProjectDetail/>}/>
         <Route path="/Register" element={<Register />} />
       </Routes>
     </BrowserRouter>
