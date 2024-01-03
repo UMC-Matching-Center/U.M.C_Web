d5c96887d318f08bd89dce752a98f5b782fb06b0
Chung Hyunwoo <woo980711@naver.com>
2024-01-03 12:38:33 +0900

    🎨 Design: Nav 위치, 페이지 배경색 설정
    Nav postion을 absolute로 지정.
    배경색 131313으로 설정.
    
    Ref: #6

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
 import { Route, Routes, BrowserRouter, Link } from "react-router-dom";
 import styled from "styled-components";
 import Home from "./pages/Home";
-import Register from "./pages/register_pages/Register";
-import Signup from "./pages/register_pages/Signup";
-import AdminSignup from "./pages/register_pages/AdminSignup";
-import UserSignup from "./pages/register_pages/UserSignup";
-import SignupSuccess from "./pages/register_pages/SignupSuccess";
+import Register from "./pages/register/Register";
 function App() {
   const Headers = styled.div`
     display: flex;
+    position: absolute;
+    z-index: 1;
   `;
 
   return (
-    <BrowserRouter>
+    <BrowserRouter style={{ position: "relative" }}>
       <Headers>
-        <Link to="/register">Home</Link>
+        <Link to="/">Home</Link>
       </Headers>
       <Routes>
-        <Route path="/*" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/register/signup" element={<Signup />} />
-        <Route path="/register/signup/mode1" element={<UserSignup />} />
-        <Route path="/register/signup/mode2" element={<AdminSignup />} />
-        <Route path="/registerComplete" element={<SignupSuccess />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/register/*" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
