c2ef9f3f38f291c8358a061cd3d4474be5ac3cf1
odukong <ohbin1017@gmail.com>
2024-01-11 00:03:04 +0900

    :sparkles: Dev: 마이페이지 및 정보 수정 페이지 구현
    (관리자, 유저) 마이페이지 및 정보 수정 페이지 구현
    App.js 내 라우팅 경로 수정
    
    Resolves: #13

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Navbar from "./common/Navbar/Navbar";
 import Home from "./pages/Home";
 import Register from "./pages/register/Register";
 import ProjectDetail from "./components/ProjectDetail";
-import MyPage from "./pages/MyPage";
+import MyPage from "./pages/myPage/MyPage";
 
 function App() {
   const Headers = styled.div`
@@ -23,7 +23,7 @@ function App() {
         <Route path="/*" element={<Home />} />
         <Route path="/project/*" element={<ProjectDetail />} />
         <Route path="/register/*" element={<Register />} />
-        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/mypage/*" element={<MyPage />} />
       </Routes>
     </BrowserRouter>
   );
