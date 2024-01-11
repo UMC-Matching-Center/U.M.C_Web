866fbf934a4ab46785e11c7af54fdfbd12eedb12
hhanoo <woo980711@naver.com>
2024-01-11 21:01:33 +0900

    ✨ 회원가입 정보 (이메일, 학교) 추가 및 정보별 입력 조건 설정
    이메일 영역 추가.
    이름, 닉네임 한글 입력 제한 기능 추가.
    전화번호 숫자 입력 제한 및 자동 하이픈 기능 추가.
    커스텀 select 디자인 학교, 기수, 파트 적용.
    
    fixes: #10

--- src/pages/register/SignupDetail.jsx
+++ src/pages/register/SignupDetail.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import "./Register.css";
@@ -25,21 +25,24 @@ const EmailInputArea = styled.div`
 const EmailAuth = styled.button`
   border: 1px solid #6b6880;
   background-color: #fafafa00;
+  font-family: "KBO Dia Gothic";
   font-size: 1rem;
+  font-weight: 300;
   color: #6b6880;
   box-sizing: border-box;
   width: 2.6rem;
-  padding: 0;
+  padding: 0.3rem;
   margin-left: 0.6rem;
 `;
 
-const SchoolAndPartArea = styled.div`
+const YearAndPartArea = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
 `;
 
 const DropDownArea = styled.div`
+  position: relative;
   min-width: 13rem;
 `;
 
@@ -48,44 +51,90 @@ const DropDownArrow = btoa(`
     <path d="M1 1L5 5L9 1" stroke="#6B6880" stroke-linecap="round"/>
   </svg>`);
 
-const DropDownSelect = styled.select`
-  border: none;
-  outline: none;
-  font-family: KBO-Dia-Gothic;
-  font-weight: 300;
-  font-size: 1.2rem;
-  color: #6b6880;
-  background-color: #fafafa00;
-  font-style: normal;
-  text-align: left;
+const SelectArrow = btoa(`
+  <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+    <g clip-path="url(#clip0_1541_5859)">
+      <path d="M2.91699 6.99998L5.83366 9.91665L11.667 4.08331" stroke="#0261AA" stroke-linecap="round" stroke-linejoin="round"/>
+    </g>
+    <defs>
+      <clipPath id="clip0_1541_5859">
+        <rect width="14" height="14" fill="white"/>
+      </clipPath>
+    </defs>
+  </svg>`);
+
+const SelectBox = styled.div`
+  box-sizing: border-box;
   width: 100%;
+  cursor: pointer;
+  margin: 0;
   padding: 0 0.5rem;
-  -o-appearance: none;
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  appearance: none;
   background: url("data:image/svg+xml;base64, ${DropDownArrow}") no-repeat right
     0.5rem center/0.8rem 0.6rem;
 `;
 
-const DropDownOption = styled.option`
-  font-style: normal;
-  font-weight: 300;
-  line-height: normal;
+const SelectOptions = styled.ul`
+  z-index: 1;
+  box-sizing: border-box;
+  position: absolute;
+  list-style: none;
+  left: 0;
+  width: 100%;
+  max-height: 11.5rem;
   padding: 0;
+  margin: 0.4rem 0 0 0;
+  overflow-y: auto; /* 스크롤이 필요한 경우만 보이도록 설정 */
+  background-color: #fafafa;
+  border-width: 0.5px 1px 1px 1px;
+  border-style: solid;
+  border-color: #6b6880;
+  display: ${(props) => (props.visible === "true" ? "block" : "none")};
+`;
+
+const Option = styled.li`
+  font-size: 1rem;
+  padding: 0.5rem;
+  box-sizing: border-box;
+  border-bottom: 0.5px solid #6b6880;
+  &:last-child {
+    // 마지막 옵션은 border-bottom 없애기
+    border-bottom: none;
+  }
+  &:hover,
+  &.selected:hover {
+    // 마우스 올려뒀을때
+    background-color: #e7e6ea;
+  }
+  &.selected {
+    background: url("data:image/svg+xml;base64, ${SelectArrow}") no-repeat right
+      0.5rem center;
+  }
 `;
 
 export default function UserSignup() {
   const [email, setEmail] = useState(""); //Email 세팅
   const [emailValid, setEmailValid] = useState(-1); //Email 유효 (-1: 초기 설정 / 0:[형식 X •인증 X] / 1:[형식 O•인증 X] / 2:[형식 O•인증 O])
-  const [emailAuth, setEmailAuth] = useState(false); //ID Auth 확인
+  const [emailAuth, setEmailAuth] = useState(false); //Email Auth 확인
+
   const [name, setName] = useState(""); //이름
   const [nickname, setNickname] = useState(""); //닉네임
-  const [year, setYear] = useState("기수"); //기수
-  const [part, setPart] = useState("파트"); //파트
+
+  const [school, setSchool] = useState(["학교", -1]); //학교
+  const [schoolOptionVisible, setSchoolOptionVisible] = useState(false); //학교 option 보이기 여부
+  const schoolSelectRef = useRef(null);
+
+  const [year, setYear] = useState(["기수", -1]); //기수
+  const [yearOptionVisible, setYearOptionVisible] = useState(false); //학교 option 보이기 여부
+  const yearSelectRef = useRef(null);
+
+  const [part, setPart] = useState(["파트", -1]); //파트
+  const [partOptionVisible, setPartOptionVisible] = useState(false); //학교 option 보이기 여부
+  const partSelectRef = useRef(null);
+
   const [phoneNumber, setPhoneNumber] = useState(""); //전화번호
   const [portfolio, setPortfolio] = useState(""); //포트폴리오 링크
   const [ableBtn, setAbleBtn] = useState(false); //회원가입 버튼
+
   const navigate = useNavigate();
 
   /* ---- Email 관련 ----- */
@@ -113,26 +162,75 @@ export default function UserSignup() {
     else setEmailValid(1); // (1: 형식 일치•비인증)
   };
 
+  /* ---- 이름•닉네임 관련 ----- */
+  const KoreanValid = (text, type) => {
+    const koreanRegex = /^[가-힣ㄱ-하-ㅣ]*$/;
+    if (!koreanRegex.test(text)) return;
+    else {
+      type == "name" ? setName(text) : setNickname(text);
+    }
+  };
+
+  /* ---- select 관련 ----- */
+  useEffect(() => {
+    // 특정 영역 외 클릭 시 발생하는 이벤트
+    const handleOutsideClick = (event, ref, setVisibility) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setVisibility(false);
+      }
+    };
+
+    // 이벤트 리스너에 handleFocus 함수 등록
+    document.addEventListener("mousedown", (event) => {
+      handleOutsideClick(event, schoolSelectRef, setSchoolOptionVisible);
+      handleOutsideClick(event, yearSelectRef, setYearOptionVisible);
+      handleOutsideClick(event, partSelectRef, setPartOptionVisible);
+    });
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+    };
+  }, [schoolSelectRef, yearSelectRef, partSelectRef]);
+
+  // 학교 option들 정의
+  const SchoolDummy = [
+    { key: "Gachon", name: "가천대학교" },
+    { key: "Ajou", name: "아주대학교" },
+    { key: "Catholic", name: "가톨릭대학교" },
+    { key: "Inha", name: "인하대학교" },
+  ];
+
   //기수 option들 정의
   const YearDummy = [
-    { value: "1st", name: "1기" },
-    { value: "2nd", name: "2기" },
-    { value: "3rd", name: "3기" },
-    { value: "4th", name: "4기" },
-    { value: "5th", name: "5기" },
+    { key: "1st", name: "1기" },
+    { key: "2nd", name: "2기" },
+    { key: "3rd", name: "3기" },
+    { key: "4th", name: "4기" },
+    { key: "5th", name: "5기" },
   ];
 
   //파트들 정의
-  const Parts = [
-    { value: "Android", name: "Android" },
-    { value: "IOS", name: "IOS" },
-    { value: "Web", name: "Web(React)" },
-    { value: "ServerNode", name: "Server(Node)" },
-    { value: "ServerSpring", name: "Server(Spring)" },
-    { value: "Design", name: "Design" },
-    { value: "Plan", name: "Plan" },
+  const PartsDummy = [
+    { key: "Android", name: "Android" },
+    { key: "IOS", name: "IOS" },
+    { key: "Web", name: "Web(React)" },
+    { key: "ServerNode", name: "Server(Node)" },
+    { key: "ServerSpring", name: "Server(Spring)" },
+    { key: "Design", name: "Design" },
+    { key: "Plan", name: "Plan" },
   ];
 
+  //전화번호 정규식 및 '-' 자동 추가
+  const validTelephone = (e) => {
+    setPhoneNumber(
+      e.target.value
+        .replace(/[^0-9]/g, "")
+        .replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/, "$1-$2-$3")
+        .replace(/(-{1,2})$/, "")
+    );
+  };
+
+  /* ---- 회원가입 버튼 관련 ----- */
   useEffect(() => {
     //이메일, 이름, 닉네임, 전화번호, 기수, 파트가 모두 정상일 때 버튼 활성화
     setAbleBtn(
@@ -169,7 +267,7 @@ export default function UserSignup() {
           <div className="FormTitle">회원가입</div>
 
           {/* 일반 유저 회원가입 폼 */}
-          <div className="FormDetail">
+          <div className="FormDetail" style={{ marginTop: "5.3rem" }}>
             <div style={{ width: "100%" }}>
               {/* 이메일(아이디) 관련 */}
               <EmailArea>
@@ -209,7 +307,7 @@ export default function UserSignup() {
                 name="text"
                 placeholder="이름"
                 value={name}
-                onChange={(e) => setName(e.target.value)}
+                onChange={(e) => KoreanValid(e.target.value, "name")}
               />
               <div className="FormInputUnderline" />
               <InputGap />
@@ -220,57 +318,78 @@ export default function UserSignup() {
                 type="text"
                 placeholder="닉네임"
                 value={nickname}
-                onChange={(e) => setNickname(e.target.value)}
+                onChange={(e) => KoreanValid(e.target.value, "nickname")}
               />
               <div className="FormInputUnderline" />
               <InputGap />
 
+              {/* 학교 부분 */}
+              <SelectBox
+                onClick={() => setSchoolOptionVisible(!schoolOptionVisible)}
+                ref={schoolSelectRef}
+              >
+                <label>{school[0]}</label>
+                <SelectOptions visible={schoolOptionVisible.toString()}>
+                  {SchoolDummy.map((option, i) => (
+                    <Option
+                      onClick={() => setSchool([option.name, i])}
+                      key={option.key}
+                      className={i === school[1] ? "selected" : ""}
+                    >
+                      {option.name}
+                    </Option>
+                  ))}
+                </SelectOptions>
+              </SelectBox>
+              <div className="FormInputUnderline" />
+              <InputGap />
+
               {/* 기수, 파트 부분 */}
-              <SchoolAndPartArea>
+              <YearAndPartArea>
+                {/* 기수 */}
                 <DropDownArea>
-                  {/* 기수 */}
-                  <DropDownSelect
-                    value={year}
-                    onChange={(e) => setYear(e.target.value)}
+                  <SelectBox
+                    onClick={() => setYearOptionVisible(!yearOptionVisible)}
+                    ref={yearSelectRef}
                   >
-                    <DropDownOption disabled selected>
-                      기수
-                    </DropDownOption>
-                    <DropDownOption disabled>------</DropDownOption>
-                    {YearDummy.map((option) => (
-                      <DropDownOption key={option.value} value={option.value}>
-                        {option.name}
-                      </DropDownOption>
-                    ))}
-                  </DropDownSelect>
-                  <div
-                    className="FormInputUnderline"
-                    style={{ minHeight: "0" }}
-                  />
+                    <label>{year[0]}</label>
+                    <SelectOptions visible={yearOptionVisible.toString()}>
+                      {YearDummy.map((option, i) => (
+                        <Option
+                          onClick={() => setYear([option.name, i])}
+                          key={option.key}
+                          className={i === year[1] ? "selected" : ""}
+                        >
+                          {option.name}
+                        </Option>
+                      ))}
+                    </SelectOptions>
+                  </SelectBox>
+                  <div className="FormInputUnderline" />
                 </DropDownArea>
 
                 {/* 파트 */}
                 <DropDownArea>
-                  <DropDownSelect
-                    value={part}
-                    onChange={(e) => setPart(e.target.value)}
+                  <SelectBox
+                    onClick={() => setPartOptionVisible(!partOptionVisible)}
+                    ref={partSelectRef}
                   >
-                    <DropDownOption disabled selected>
-                      파트
-                    </DropDownOption>
-                    <DropDownOption disabled>------</DropDownOption>
-                    {Parts.map((option) => (
-                      <DropDownOption key={option.value} value={option.value}>
-                        {option.name}
-                      </DropDownOption>
-                    ))}
-                  </DropDownSelect>
-                  <div
-                    className="FormInputUnderline"
-                    style={{ minHeight: "0" }}
-                  />
+                    <label>{part[0]}</label>
+                    <SelectOptions visible={partOptionVisible.toString()}>
+                      {PartsDummy.map((option, i) => (
+                        <Option
+                          onClick={() => setPart([option.name, i])}
+                          key={option.key}
+                          className={i === part[1] ? "selected" : ""}
+                        >
+                          {option.name}
+                        </Option>
+                      ))}
+                    </SelectOptions>
+                  </SelectBox>
+                  <div className="FormInputUnderline" />
                 </DropDownArea>
-              </SchoolAndPartArea>
+              </YearAndPartArea>
               <InputGap />
 
               {/* 전화번호 부분 */}
@@ -279,7 +398,8 @@ export default function UserSignup() {
                 type="tel"
                 placeholder="전화번호"
                 value={phoneNumber}
-                onChange={(e) => setPhoneNumber(e.target.value)}
+                onChange={(e) => validTelephone(e)}
+                maxLength={13}
               />
               <div className="FormInputUnderline" />
               <InputGap />
