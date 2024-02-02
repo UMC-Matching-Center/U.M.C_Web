cf668eb8a47b0a0d1009a7537d8615a875fc6b3c
XinguOh <ojyoung2490@gmail.com>
2024-02-03 02:51:37 +0900

    ♻️ Refactor: 지원 현황 페이지 구현 - 지원 현황 페이지 모든 기능 구현 완료 - PMNavBar에 해당 지원 현황 페이지 링크 연결
    Resolves: #38
    Ref : #20

---
+++ src/components/viewstatus/AlertModal.jsx
@@ -0,0 +1,114 @@
+import React from "react";
+import styled from "styled-components";
+import "../Modal.css";
+
+//바탕화면 컬러 변경하기
+const Backdrop = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5); /* Adjust the opacity as needed */
+  z-index: 1;
+  display: ${(props) => (props.show ? "block" : "none")};
+`;
+
+//모달 위치 설정
+const ModalBoxContainer = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 2;
+`;
+
+//느낌표 표시
+const FailIcon = btoa(`
+<svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 70 70" fill="none">
+  <g clip-path="url(#clip0_2607_20911)">
+    <path d="M35 55.416V55.4453" stroke="#FAFAFA" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M35 43.7506V14.584" stroke="#FAFAFA" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"/>
+  </g>
+  <defs>
+    <clipPath id="clip0_2607_20911">
+      <rect width="70" height="70" fill="white"/>
+    </clipPath>
+  </defs>
+</svg>
+`);
+
+//체크 표시
+const CheckIcon = btoa(`
+  <svg xmlns="http://www.w3.org/2000/svg" width="70" height="70" viewBox="0 0 70 70" fill="none">
+    <g clip-path="url(#clip0_2458_10361)">
+      <path d="M11.6666 31.1112L29.1666 49.5834L58.3333 20.4167" stroke="#FAFAFA" stroke-width="5" stroke-linecap="round" stroke-linejoin="round"/>
+    </g>
+    <defs>
+      <clipPath id="clip0_2458_10361">
+        <rect width="70" height="70" fill="white"/>
+      </clipPath>
+    </defs>
+  </svg>`);
+
+//체크 아이콘 표시 공간
+const CheckIconArea = styled.div`
+  position: absolute;
+  width: 7rem;
+  height: 7rem;
+  background: url("data:image/svg+xml;base64, ${(props) =>
+      props.modal ? CheckIcon : FailIcon}")
+    no-repeat;
+`;
+
+export default function AlertModal({
+  showModal,
+  setShowModal,
+  modalType,
+  selectedAppName,
+  handlePassOrFail,
+}) {
+  return (
+    <>
+      <Backdrop show={showModal} />
+      <ModalBoxContainer>
+        <div className="ModalBox">
+          <div className="IconCircleBackground">
+            <div
+              className="IconCircleGreen"
+              style={
+                modalType == "불합격" ? { backgroundColor: "#D62117" } : {}
+              }
+            >
+              <CheckIconArea modal={modalType == "합격"} />
+            </div>
+          </div>
+          <div className="ModalConentBox">
+            <div className="MoalContentCenter">
+              <div className="ModalInfo" style={{ margin: "11.5rem 0 0 0" }}>
+                첼린저 {selectedAppName}를 {modalType} 처리하시겠습니까?
+              </div>
+              <div className="ModalBtnArea" style={{ margin: "8.5rem 0 0 0" }}>
+                <div
+                  className="ModalBtnWhite"
+                  onClick={() => setShowModal(false)}
+                >
+                  취소
+                </div>
+                <div
+                  className="ModalBtnGreen"
+                  style={
+                    modalType == "불합격" ? { backgroundColor: "#D62117" } : {}
+                  }
+                  onClick={() => handlePassOrFail(selectedAppName)}
+                >
+                  {modalType}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </ModalBoxContainer>
+    </>
+  );
+}
--- src/pages/myProject/viewstatus/ViewStatus.jsx
+++ src/pages/myProject/viewstatus/ViewStatus.jsx
@@ -1,49 +1,71 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import ViewHeader from "../../../components/viewstatus/ViewHeader";
-const ViewContainer = styled.div`
+import ViewBody from "../../../components/viewstatus/ViewBody";
+import ViewSelect from "../../../components/viewstatus/ViewSelect";
+
+//총 컨테이너
+const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
   flex-direction: column;
+  color: #fafafa;
+  font-family: KBO-Dia-Gothic;
 `;
 
-const ViewHeadContainer = styled.div`
+//헤더 컨테이너
+const HeadContainer = styled.div`
   display: flex;
   width: 99.2rem;
   height: 16.9rem;
   border-radius: 1rem;
   background-color: #fafafa;
   color: #fafafa;
+  justify-content: center;
 `;
 
-const ViewApplicantHeader = styled.div`
+//지원자 보기 헤더
+const ApplicantHeader = styled.div`
   margin-top: 8.6rem;
   height: 6rem;
   width: 99.2rem;
   border-bottom: 1px solid #fafafa;
+  display : flex;
+  flex-direction : row;
 `;
-const ViewApplicantText = styled.div`
+
+//지원자 보기 헤더 텍스트
+const ApplicantText = styled.div`
   font-size: 2.4rem;
   font-weight: 500;
   line-height: 150%;
   color: #fafafa;
 `;
 
-const ViewApplicantBody = styled.div`
-  margin: 3.4rem 0 0 3.6rem;
+//지원자보기 해당 바디 큰 틀
+const ApplicantContainer = styled.div`
+  width: 99.2rem;
+  margin: 3.4rem 0 10rem 3.6rem;
 `;
 
+
 export default function ViewStatus() {
+  //선택한옵션 설정
+  const [selectedOptionIndex, setSelectedOptionIndex] = useState("전체보기"); 
+  
   return (
-    <ViewContainer>
-      <ViewHeadContainer>
+    <Container>
+      <HeadContainer>
         <ViewHeader />
-      </ViewHeadContainer>
-      <ViewApplicantHeader>
-        <ViewApplicantText>지원자 보기</ViewApplicantText>
-      </ViewApplicantHeader>
-      <ViewApplicantBody></ViewApplicantBody>
-    </ViewContainer>
+      </HeadContainer>
+      <ApplicantHeader>
+        <ApplicantText>지원자 보기</ApplicantText>
+        <ViewSelect setSelectedOptionIndex={setSelectedOptionIndex}/>
+      </ApplicantHeader>
+      <ApplicantContainer>
+        <ViewBody selectedOptionIndex={selectedOptionIndex}/>
+      </ApplicantContainer>
+    </Container>
   );
 }
