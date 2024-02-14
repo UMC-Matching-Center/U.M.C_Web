66f1f7320f2b9641447f71aed9d89997581f0324
odukong <ohbin1017@gmail.com>
2024-02-14 17:05:45 +0900

    :sparkles: Dev: 랜딩페이지 기본 UI 구현
    - 랜딩 페이지 및 홈 페이지 디테일에 사용되는 MemberCard 구현
    - 랜딩 상세 페이지 구현 (LandingPage - LandingPageView)
    
    Related to: #46

--- src/pages/myProject/MyProject.jsx
+++ src/pages/myProject/MyProject.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import ReviewTeam from "./review/ReviewTeam";
+import LandingPage from "./LandingPage";
 
 export default function MyProject() {
-    return(
-        <Routes>
-        <Route path="/review" exact element={<ReviewTeam />}></Route>
-      </Routes>
-    )
+  return (
+    <Routes>
+      <Route path="/review" exact element={<ReviewTeam />}></Route>
+      <Route path="/landing/*" element={<LandingPage />}></Route>
+    </Routes>
+  );
 }
